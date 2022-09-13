a4c70ab4a9ea97783515620d23b2e5ea37e0e88a
stdimitrov05 <stdimitrov05@gmail.com>
2022-09-13 16:47:49 +0300

    Add files

---
+++ index.js
@@ -0,0 +1,40 @@
+const  {ERRORS_COMMAND_NOT_FOUND} = require('./dev/errors')
+
+const {Client, GatewayIntentBits} = require('discord.js');
+const client = new Client({intents: [GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,]});
+require('dotenv').config()
+
+let prefix = process.env.DISCORD_CMD_START
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate' , (msg) =>{
+    if (msg.author.bot) return  ;
+
+    //Starting with prefix
+    let args =  msg.content.slice(prefix.length).trim().split(" ").shift()
+    // Command to lowerCase
+    let cmd =  args.toLowerCase()
+
+    // Commands
+    switch (cmd) {
+        case "hello" :
+            msg.reply(`Hello World ${msg.author.tag} !`)
+            break
+        case "age" :
+            break
+        default : msg.reply(`Status error: ` + `Command not found \n error: ` +  ERRORS_COMMAND_NOT_FOUND)
+            break
+    }
+
+
+} )
+
+
+client.login(process.env.DISCORD_TOKEN);
